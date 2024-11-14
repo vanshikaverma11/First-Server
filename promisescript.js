10d47218f498fe9ec51ad420301e5aee640a8728
vanshikav4567 <vanshikav4567@gmail.com>
2024-11-14 09:31:03 +0000

    addnew changes call 2nd promise first and 1st in last

--- promisescript.js
+++ promisescript.js
@@ -1,4 +1,4 @@
-//Creating a promise method. The promise will get resolved when timer times out after 6 seconds.
+//Creating a promise method. The promise will get resolved when timer times out after 6 seconds and 3 second.
 let myPromise1 = new Promise((resolve,reject) => {
     setTimeout(() => {
       resolve("Promise1 resolved")
@@ -9,10 +9,11 @@ let myPromise2 = new Promise((resolve,reject) => {
       resolve("Promise2 resolved")
     },3000)})
 
-myPromise1.then((successMessage) => {
-    console.log("From Callback " + successMessage)
-myPromise2.then((successMessage) => {
-    console.log("From Callback " + successMessage)
-  })
+    myPromise1.then((successMessage) => {
+        console.log("From Callback " + successMessage)
+    })
+    myPromise2.then((successMessage) => {
+        console.log("From Callback " + successMessage)
+
     
-  })
+})
